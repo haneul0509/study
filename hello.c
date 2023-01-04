9f5c47c8a9197210a961b3898a555d0dd3ba47e5
haneul <haneul0509@naver.com>
2023-01-04 22:06:55 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,8 @@
+
+
+int main(void){
+
+printf("why?");
+
+return 0;
+}
---
+++ 신고 결과 받기(1/2-1/4).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void){
+
+string s;
+cout<<"왜그래여"<<endl;
+cin>>s;
+
+}
